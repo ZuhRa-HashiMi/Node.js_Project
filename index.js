44da9e69c6abd133b097542c33fca29279744807
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-19 12:08:51 +0430

    Reading and Writing Files

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const hello = "Hello World";
-console.log(hello);
+const fs = require('fs');
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `this is what we want to know about Zuhra Hashimi: ${textIn}.\ncreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('file written successfully');
