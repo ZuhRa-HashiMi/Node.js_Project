987e78fd8e75b94429d80e8b1cc4d20a1bccd79b
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2024-03-20 12:18:22 +0430

    HTML Templating: Filling the Templates

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+// const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-/////////////////////////////////
-// Server
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-  const dataOj= JSON.parse(data);
-
-
-const server=http.createServer((req, res) => {
-  const patName = req.url;
-
-  if ( patName === '/' || patName ==='/overview') {
-    res.end('This is from overview!');
-  } else if (patName === '/product') {
-    res.end('This is from product!');
-  } else if (patName === '/api') {
-      res.writeHead(200, {
-        'content-type':'application/json'
-      });
-      res.end(data);
-    
-  } else {
-    res.writeHead(404, {
-      'content-type':'text/html',
-      'my-own-header': 'Hello world'
-    });
-    res.end('<h1>The page not found</h1>');
-  }
-
-
-});
-
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
-});
 /////////////////////////////////
 // FILES
 
@@ -45,7 +14,7 @@ server.listen(8000, '127.0.0.1', () => {
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
-// // Non-blocking, asynchronous way
+// Non-blocking, asynchronous way
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //   if (err) return console.log('ERROR! 💥');
 
@@ -62,4 +31,66 @@ server.listen(8000, '127.0.0.1', () => {
 // });
 // console.log('Will read file!');
 
+/////////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+// console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
